f2f7c2c6b197ac48b46b7e72f59c6cdad2835537
MavieGale <b2029mggcruz@pshs.edu.ph>
2026-01-30 11:29:51 +0800

    fixed debug

--- public/q3/script.js
+++ public/q3/script.js
@@ -1,48 +1,52 @@
-// Get the signup form
-const form = document.getElementById("dForm");
-
-if (form) {
-
-
-// Run when user clicks Accept
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); // prevent page reload
-
-  // Ask confirmation before saving
-  if (!confirm("Are you sure you want to submit?")) return;
-
-  // Read existing accounts from localStorage
-  let saved = localStorage.getItem("accounts");
-
-  // If none yet, start with empty array
-  let accounts = saved ? JSON.parse(saved) : [];
-
-  // Collect all form inputs
-  const student = {
-    sId: document.getElementById("sId").value,
-    fname: document.getElementById("fname").value,
-    dob: document.getElementById("dob").value,
-    emad: document.getElementById("emad").value,
-    gradelvl: document.getElementById("gradelvl").value,
-    club: document.getElementById("club").value,
-    "i/e": document.querySelector('input[name="i/e"]:checked').value
-  };
-  // Add new signup to array
-  accounts.push(student);
-
-  // Save updated array back to localStorage
-  localStorage.setItem("accounts", JSON.stringify(accounts));
-
-  alert("Signup saved successfully!");
-
-  // Clear form after saving
-  form.reset();
-});
-
-// Ask confirmation before clearing form
-form.addEventListener("reset", function (e) {
-  if (!confirm("Clear the form?")) {
-    e.preventDefault();
-  }
-});
+// read from the localStorage saved as a string - to see if there are anything saved on the users computer
+let acctString = localStorage.getItem("accounts");
+if (!acctString) {
+accountList = {};
+} else {
+accountList = JSON.parse(acctString);
 }
+
+const form = document.getElementById("dForm"); // get the HTML form
+
+// event handler on the submit button
+form.addEventListener("submit", function (e) {
+e.preventDefault(); // prevent page reload
+
+if (confirm("Are you sure you want to proceed?")) {
+const data = new FormData(form);
+const obj = Object.fromEntries(data.entries());
+
+// USE Student ID as the key instead of uname
+accountList[obj.sId] = {};
+
+for (let key in obj) {
+if (key !== "sId") {
+accountList[obj.sId][key] = obj[key];
+}
+}
+
+console.log(accountList);
+acctString = JSON.stringify(accountList);
+localStorage.setItem("accounts", acctString);
+
+form.submit();
+}
+});
+
+// event handler for the reset button
+form.addEventListener("reset", function (e) {
+if (!confirm("Are you sure you want to clear the form?")) {
+e.preventDefault();
+}
+});
+
+
+function focusColor(ele) {
+  console.log(ele);
+  ele.style.backgroundColor = "light blue";
+} 
+
+function blurColor(ele) {
+  console.log(ele);
+ ele.style.backgroundColor = "white";
+} 
